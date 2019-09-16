b7b59b11c97b6dad9dd605b2cf24b3b339e1dab6
AllyHyeseongKim <ally3k@icloud.com>
2019-09-16 21:44:49 +0900

    Update makefile and redirection

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-
+var qs = require('querystring');
+ 
 function templateHTML(title, list, body){
   return `
   <!doctype html>
@@ -19,7 +20,6 @@ function templateHTML(title, list, body){
   </html>
   `;
 }
-
 function templateList(filelist){
   var list = '<ul>';
   var i = 0;
@@ -27,18 +27,16 @@ function templateList(filelist){
     list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
     i = i + 1;
   }
-  list = list + '</ul>';
+  list = list+'</ul>';
   return list;
 }
-
+ 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    console.log(pathname);
-    if(pathname === '/') {
+    if(pathname === '/'){
       if(queryData.id === undefined){
-
         fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
@@ -46,52 +44,56 @@ var app = http.createServer(function(request,response){
           var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200);
           response.end(template);
-        })   
+        });
       } else {
         fs.readdir('./data', function(error, filelist){
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
-            list = templateList(filelist);
+            var list = templateList(filelist);
             var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
             response.writeHead(200);
-            response.end(template); 
+            response.end(template);
           });
+        });
+      }
+    } else if(pathname === '/create'){
+      fs.readdir('./data', function(error, filelist){
+        var title = 'WEB - create';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `
+          <form action="http://localhost:3000/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `);
+        response.writeHead(200);
+        response.end(template);
       });
+    } else if(pathname === '/create_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var title = post.title;
+          var description = post.description;
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end('success');  
+          })
+    });
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
     }
-  } else if(pathname === '/create'){
-    fs.readdir('./data', function(error, filelist){
-      var title = 'WEB - create';
-      var list = templateList(filelist);
-      var template = templateHTML(title, list, `
-      <form action="http://localhost:3000/create_process" method="post">
-        <p><input type="text" placeholder="title"></p>
-        <p>
-            <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-            <input type="submit">
-        </p>
-      </form>
-      `);
-      response.writeHead(200);
-      response.end(template);
-    })
-  } else if(pathname === '/create_process'){
-    var body = '';
-    request.on('data', function(data){
-      body = body + data;
-    });
-    request.on('end', function(){
-      var post = qs.parse(body);
-      var title = post.title;
-      var description = post.description;
-      console.log(post.title);
-    });
-    response.writeHead(200);
-    response.end('success');
-  } else {
-    response.writeHead(404);
-    response.end('Not found');
-  }
+ 
+ 
+ 
 });
 app.listen(3000);
