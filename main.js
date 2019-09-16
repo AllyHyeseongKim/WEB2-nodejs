16151bfe306b39cc0cdf97a542d87ed47ce9c0bd
AllyHyeseongKim <ally3k@icloud.com>
2019-09-16 22:26:15 +0900

    Update delete

--- main.js
+++ main.js
@@ -138,7 +138,19 @@ var app = http.createServer(function(request,response){
               response.end();
             })
           })
-          console.log(post);
+        });
+    } else if(pathname === '/delete_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          fs.unlink(`data/${id}`, function(error){
+            response.writeHead(302, {Location: `/`});
+            response.end();
+          })
         });
     } else{
       response.writeHead(404);
