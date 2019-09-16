e931e1aad9b79dd2ee47463ae4179ee00cd53829
AllyHyeseongKim <ally3k@icloud.com>
2019-09-16 21:34:56 +0900

    Update get POST data

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ var app = http.createServer(function(request,response){
       var title = 'WEB - create';
       var list = templateList(filelist);
       var template = templateHTML(title, list, `
-      <form action="http://localhost:3000/process_create" method="post">
+      <form action="http://localhost:3000/create_process" method="post">
         <p><input type="text" placeholder="title"></p>
         <p>
             <textarea name="description" placeholder="description"></textarea>
@@ -76,6 +76,19 @@ var app = http.createServer(function(request,response){
       response.writeHead(200);
       response.end(template);
     })
+  } else if(pathname === '/create_process'){
+    var body = '';
+    request.on('data', function(data){
+      body = body + data;
+    });
+    request.on('end', function(){
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      console.log(post.title);
+    });
+    response.writeHead(200);
+    response.end('success');
   } else {
     response.writeHead(404);
     response.end('Not found');
