b6d96ed4f39d04944b8c9ccb257e0c6ae31190bb
AllyHyeseongKim <ally3k@icloud.com>
2019-09-16 22:30:33 +0900

    Update object syntex

---
+++ syntex/object.js
@@ -0,0 +1,9 @@
+var members = ['Ally', 'Hyeseong', 'Kim'];
+console.log(members[1]);    // Hyeseong
+
+var roles = {
+    'programmer':'Ally',
+    'designer' : 'Hyeseong',
+    'manager' : 'Kim'
+}
+console.log(roles.designer);    // Hyeseong
